fb152a16cb4d0e8ab8c7e443c7eb29edf3ca0def
FURRKA <alexsey.furrka@gmail.com>
2025-04-03 12:07:48 +0300

    Изменения основного контекста данных. Добавлена фабрика контекста данных

---
+++ DAL/DBContext/ApplicationDBContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DAL.DBContext
+{
+    public class ApplicationDBContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    {
+        public ApplicationDBContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            optionBuilder.UseNpgsql("Host=localhost;Port=5432;Database=WTM;Username=postgres;Password=123;");
+            return new ApplicationDBContext(optionBuilder.Options);
+        }
+    }
+}
